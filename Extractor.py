c0a683c6fcccc45cd76976348558c838d1b28634
Rex <JFRHuggins@gmail.com>
2020-09-06 18:42:35 -0700

    User Input Addition
    Added functionality to allow user input of Steam Workshop Collection ID  number, along with input validation.

--- Extractor.py
+++ Extractor.py
@@ -1,20 +1,34 @@
-## Script will output a total of discovered mods
-## Script will output the stored location of the output file
-## TO DO: Remove hardcoded page variable, rename and allow user input of steam workshop ID OR URL
+## The Steam Workshop Collection Modlist Extractor (SWCME) is intended to extract the mod ID's from workshop mod collections.
+## The SWCME will accept the collection ID or it's URL, and process the page to create a .csv file of comma separated mod ID's.
+## The SWCME will output the 'modlist.csv' to the current working directory of the script, as well as print it in the terminal.
 
 from lxml import html
 import requests
 
-page = requests.get('https://steamcommunity.com/sharedfiles/filedetails/?id=1724309839')
+input('Welcome to the SWCME, press Enter to continue...')
+
+modid = int(0)
+while True:
+    try:
+        modid = int(input('Please provide the Steam Workshop Collection ID number. [Integers 0-9, max length 10] '))
+    except ValueError:
+        print('ERROR!: Input was not recognised as a number.')
+    if 1 <= int(modid) <= 9999999999:
+        break
+    else:
+        print('Please Try your input again.')
+
+page = requests.get('https://steamcommunity.com/sharedfiles/filedetails/?id=' + str(modid))
 tree = html.fromstring(page.content)
 idloc = tree.xpath('//div[@id="profileBlock"]//div[contains(@class, "collectionItem") and starts-with(@id, "sharedfile_")]')
 idnum = [element.get("id").replace("sharedfile_","") for element in idloc]
 modlist = ','.join(idnum)
-filename = 'modlist.csv'   
-
+filename = 'modlist.csv'
+print('Mod Extraction complete! The following mod IDs have been located:')   
 print(modlist)
-input('Press enter to write to file and continue')
+input('Press Enter to write to file and continue.')
 outfile = open(filename,'w')
 outfile.write(modlist)
 outfile.close()
-input('File generation complete, thanks!')
+input('File generation complete, press Enter to exit.')
+quit()
